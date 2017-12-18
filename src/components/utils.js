bd7aada07c43aa9484c7c1f525e4991ba3175fa6
YanYunfei <yan@thicketlabs.com>
2017-12-18 10:43:11 -0500

    add util functions to switch between region name and region code

--- src/components/utils.js
+++ src/components/utils.js
@@ -57,3 +57,27 @@ export const segsToUrl = (urlSegs) => {
   }
   return url
 }
+
+const regionNametoCode = {
+  'High Income OECD Countries': 'OECD',
+  'Central and Eastern Europe and Central Asia': 'CEECA',
+  'East Asia and the Pacific': 'EAP',
+  'Latin America and the Caribbean': 'LAC',
+  'Middle East and Northern Africa': 'MENA',
+  'Sub-Saharan Africa': 'SSA',
+  'Civil and Political Rights Pilot Countries': 'CPRP',
+}
+
+const regionCodetoName = {
+  OECD: 'High Income OECD Countries',
+  CEECA: 'Central and Eastern Europe and Central Asia',
+  EAP: 'East Asia and the Pacific',
+  LAC: 'Latin America and the Caribbean',
+  MENA: 'Middle East and Northern Africa',
+  SSA: 'Sub-Saharan Africa',
+  CPRP: 'Civil and Political Rights Pilot Countries',
+}
+
+export const getRegionName = (regionCode) => (regionCodetoName[regionCode])
+
+export const getRegionCode = (regionName) => (regionNametoCode[regionName])
