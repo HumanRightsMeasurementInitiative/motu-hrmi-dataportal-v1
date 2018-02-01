5353414f87c62f4de3c476af4b7859a3a60f6ff6
cesare.soldini <cesare.soldini@accurat.it>
2018-02-01 14:33:08 +0100

    🐛 Fix wrong axes on CountryRightsChart radar

--- src/components/CountryRightsChart/index.js
+++ src/components/CountryRightsChart/index.js
@@ -3,6 +3,23 @@ import PropTypes from 'prop-types'
 import { PetalChart } from 'hrmi-charts'
 import rightsDefinitions from 'data/rights-definitions.json'
 
+const RIGHTS_ORDER = [
+  'health', // ESR
+  'housing', // ESR
+  'work', // ESR
+
+  'freedomFromDisappearance', // CPR
+  'freedomFromArbitraryArrest', // CPR
+  'freedomFromExecution', // CPR
+  'freedomFromTorture', // CPR
+  'participateInGovernment', // CPR
+  'assemblyAndAssociation', // CPR
+  'opinionAndExpression', // CPR
+
+  'food', // ESR
+  'education', // ESR
+]
+
 export default class CountryRightsChart extends React.Component {
   static propTypes = {
     rights: PropTypes.object.isRequired,
@@ -13,24 +30,24 @@ export default class CountryRightsChart extends React.Component {
     const { esr_hi: esrHi, esr_core: esrCore, cpr } = rights
     const esrType = 'HIGH_INCOME' // or 'CORE'
 
-    const rightsData = Object.entries(rightsDefinitions)
-      .map(([code, { type }]) => {
-        const NO_DATA = null
-        if (type === 'ESR' && esrType === 'HIGH_INCOME') {
-          if (!esrHi) return NO_DATA
-          if (esrHi[code] === null) return NO_DATA
-          return esrHi[code] / 100
-        } else if (type === 'ESR' && esrType === 'CORE') {
-          if (!esrCore) return NO_DATA
-          if (esrCore[code] === null) return NO_DATA
-          return esrCore[code] / 100
-        } else if (type === 'CPR') {
-          if (!cpr || !cpr[code]) return NO_DATA
-          if (cpr[code] === null || cpr[code].mean === null) return NO_DATA
-          return cpr[code].mean / 10
-        }
-        throw new Error('Right with no type! Check rightDefinitions')
-      })
+    const rightsData = RIGHTS_ORDER.map(rightCode => {
+      const { type } = rightsDefinitions[rightCode]
+      const NO_DATA = null
+      if (type === 'ESR' && esrType === 'HIGH_INCOME') {
+        if (!esrHi) return NO_DATA
+        if (esrHi[rightCode] === null) return NO_DATA
+        return esrHi[rightCode] / 100
+      } else if (type === 'ESR' && esrType === 'CORE') {
+        if (!esrCore) return NO_DATA
+        if (esrCore[rightCode] === null) return NO_DATA
+        return esrCore[rightCode] / 100
+      } else if (type === 'CPR') {
+        if (!cpr || !cpr[rightCode]) return NO_DATA
+        if (cpr[rightCode] === null || cpr[rightCode].mean === null) return NO_DATA
+        return cpr[rightCode].mean / 10
+      }
+      throw new Error(`Right '${rightCode}' with no type! Check rightDefinitions`)
+    })
 
     return (
       <div>
