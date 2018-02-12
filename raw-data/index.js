1690af833cfd26967e97ef484fc2defb268b5f39
cesare.soldini <cesare.soldini@accurat.it>
2018-02-12 15:43:31 +0100

    🌥 Add data elaboration for wordCloud, and the data in rights-by-country

--- raw-data/index.js
+++ raw-data/index.js
@@ -15,12 +15,21 @@ const CPR_MAPPING = {
   Mozambique: 'MOZ',
   Nepal: 'NPL',
   NewZealand: 'NZL',
+  'New Zealand': 'NZL',
   SaudiArabia: 'SAU',
+  'Saudi Arabia': 'SAU',
   UnitedKingdom: 'GBR',
+  'United Kingdom': 'GBR',
 }
 
 function round(float) {
   return parseFloat(parseFloat(float).toFixed(1))
+}
+
+function readSheet(fileName, sheetIndex = 0) {
+  const workbook = XLSX.readFile(fileName)
+  const sheet = workbook.Sheets[workbook.SheetNames[sheetIndex]]
+  return sheet
 }
 
 function sheetRows(sheet, columns = 'AZ', startFromRow = 0) {
@@ -42,11 +51,7 @@ function sheetRows(sheet, columns = 'AZ', startFromRow = 0) {
 }
 
 function ESRHighIncome() {
-  const workbook = XLSX.readFile('esr-high-income.xlsx')
-  const sheetName = workbook.SheetNames[0]
-  const sheet = workbook.Sheets[sheetName]
-
-  const rows = sheetRows(sheet, 'AS', 1).map(row => {
+  const rows = sheetRows(readSheet('esr-high-income.xlsx'), 'AS', 1).map(row => {
     const datum = {
       countryName: row.A,
       countryCode: row.C,
@@ -113,11 +118,7 @@ function ESRHighIncome() {
 }
 
 function ESRCore() {
-  const workbook = XLSX.readFile('esr-core.xlsx')
-  const sheetName = workbook.SheetNames[0]
-  const sheet = workbook.Sheets[sheetName]
-
-  const rows = sheetRows(sheet, 'AV', 1).map(row => {
+  const rows = sheetRows(readSheet('esr-core.xlsx'), 'AV', 1).map(row => {
     const datum = {
       // countryName: row.A, // Malformed for Core data
       countryCode: row.C,
@@ -223,67 +224,120 @@ function CPR() {
   return _.keyBy(countries, 'countryCode')
 }
 
-const esrHI = ESRHighIncome()
-const esrCore = ESRCore()
-const cpr = CPR()
+function CPRRangeAtRisk() {
+  const rows = d3.csvParse(fs.readFileSync('cpr-range-at-risk.csv', 'utf-8'))
+  const codebook = sheetRows(readSheet('cpr-range-at-risk-codebook.xlsx'), 'AC', 1).map(row => {
+    if (row.A === 'Country' || row.A === 'CountryCode') return null
+    if (row.C === 'Do Not Include in Online Visualizations') return null
 
-const countryCodesList = Object.keys(esrHI)
+    return {
+      wordCode: row.A.replace(`"right"_`, ''),
+      word: row.B,
+    }
+  }).filter(Boolean)
 
-const catalogCountries = []
+  const wordsValuesByCountry = {}
+  rows.forEach(row => {
+    const countryCode = CPR_MAPPING[row.Country]
+    if (!countryCode) throw new Error(`Missing CPR_MAPPING country code: '${row.Country}'`)
 
-const joinedCountries = countryCodesList.map(countryCode => {
-  const countryEsrHI = esrHI[countryCode]
-  const countryEsrCore = esrCore[countryCode]
-  const countryCpr = cpr[countryCode] || { rights: null }
+    const getWordsValues = rightCode => codebook.map(({ word, wordCode }) => {
+      const value = parseFloat(row[`${rightCode}_${wordCode}`])
+      if (!value || value === 0) return null
+      if (word === 'Reliigon') word = 'Religion' // WTF?
+      return [ word, value ]
+    }).filter(Boolean)
 
-  const countryInfo = _.pick(countryEsrHI, [
-    'countryCode',
-    'countryName',
-    // 'GDP',
-    // 'SERF',
-  ])
-  const rights = {
-    esrHI: countryEsrHI.rights,
-    esrHIHistorical: countryEsrHI.historical,
-    esrCore: countryEsrCore.rights,
-    esrCoreHistorical: countryEsrCore.historical,
-    cpr: countryCpr.rights,
-  }
+    wordsValuesByCountry[countryCode] = {
+      'opinion-and-expression': getWordsValues('express'),
+      'assembly-and-association': getWordsValues('assoc').concat(getWordsValues('assem')),
+      'assembly-and-association-sub': {
+        'assembly': getWordsValues('assem'),
+        'association': getWordsValues('assoc'),
+      },
+      'participate-in-government': getWordsValues('polpart'),
+      'freedom-from-torture': getWordsValues('tort'),
+      'freedom-from-execution': getWordsValues('exkill').concat(getWordsValues('dpex')),
+      'freedom-from-execution-sub': {
+        'freedom-from-the-death-penalty': getWordsValues('dpex'),
+        'freedom-from-extrajudicial-execution': getWordsValues('exkill'),
+      },
+      'freedom-from-arbitrary-arrest': getWordsValues('arrest'),
+      'freedom-from-disappearance': getWordsValues('disap'),
+    }
+  })
 
-  const countryCatalog = _.pick(countryEsrHI, [
-    'countryCode',
-    'isHighIncome',
-    'isOECD',
-    'geoRegion',
-  ])
-  countryCatalog.geoRegion = countryCatalog.geoRegion.replace(/_/g, '-')
-  countryCatalog.hasCPR = countryCpr.rights !== null
-  catalogCountries.push(countryCatalog)
-
-  return { ...countryInfo, rights }
-})
-
-const joinedCountriesByCountry = _.keyBy(joinedCountries, 'countryCode')
-
-const catalogCountriesByRegion = d3.nest()
-  .key(c => c.geoRegion)
-  .rollup(cs => cs.map(c => c.countryCode))
-  .object(catalogCountries)
-const catalogCountriesHIOECD = catalogCountries.filter(c => c.isHighIncome && c.isOECD).map(c => c.countryCode)
-const catalogCountriesCPRPilot = catalogCountries.filter(c => c.hasCPR).map(c => c.countryCode)
-
-const regions = {
-  ...catalogCountriesByRegion,
-  'high-income-oecd': catalogCountriesHIOECD,
-  'cpr-pilot': catalogCountriesCPRPilot,
+  return wordsValuesByCountry
 }
 
-const indent = 2
-fs.writeFileSync('../src/data/rights-by-country.json', JSON.stringify(joinedCountriesByCountry, null, indent))
-fs.writeFileSync('../src/data/country-categories.json', JSON.stringify(regions, null, indent))
+function calculate() {
+  const esrHI = ESRHighIncome()
+  const esrCore = ESRCore()
+  const cpr = CPR()
+  const cprRangeAtRisk = CPRRangeAtRisk()
 
-console.log(`
-Written data files:
-  • ../src/data/rights-by-country.json: Rights data for each country
-  • ../src/data/country-categories.json: Countries grouped by category (geographical or political)
-`)
+  const countryCodesList = Object.keys(esrHI)
+
+  const catalogCountries = []
+
+  const joinedCountries = countryCodesList.map(countryCode => {
+    const countryEsrHI = esrHI[countryCode]
+    const countryEsrCore = esrCore[countryCode]
+    const countryCpr = cpr[countryCode] || { rights: null }
+    const countryCprRangeAtRisk = cprRangeAtRisk[countryCode]
+
+    const countryInfo = _.pick(countryEsrHI, [
+      'countryCode',
+      'countryName',
+      // 'GDP',
+      // 'SERF',
+    ])
+    const rights = {
+      esrHI: countryEsrHI.rights,
+      esrHIHistorical: countryEsrHI.historical,
+      esrCore: countryEsrCore.rights,
+      esrCoreHistorical: countryEsrCore.historical,
+      cpr: countryCpr.rights,
+      cprRangeAtRisk: countryCprRangeAtRisk,
+    }
+
+    const countryCatalog = _.pick(countryEsrHI, [
+      'countryCode',
+      'isHighIncome',
+      'isOECD',
+      'geoRegion',
+    ])
+    countryCatalog.geoRegion = countryCatalog.geoRegion.replace(/_/g, '-')
+    countryCatalog.hasCPR = countryCpr.rights !== null
+    catalogCountries.push(countryCatalog)
+
+    return { ...countryInfo, rights }
+  })
+
+  const joinedCountriesByCountry = _.keyBy(joinedCountries, 'countryCode')
+
+  const catalogCountriesByRegion = d3.nest()
+    .key(c => c.geoRegion)
+    .rollup(cs => cs.map(c => c.countryCode))
+    .object(catalogCountries)
+  const catalogCountriesHIOECD = catalogCountries.filter(c => c.isHighIncome && c.isOECD).map(c => c.countryCode)
+  const catalogCountriesCPRPilot = catalogCountries.filter(c => c.hasCPR).map(c => c.countryCode)
+
+  const regions = {
+    ...catalogCountriesByRegion,
+    'high-income-oecd': catalogCountriesHIOECD,
+    'cpr-pilot': catalogCountriesCPRPilot,
+  }
+
+  const indent = 2
+  fs.writeFileSync('../src/data/rights-by-country.json', JSON.stringify(joinedCountriesByCountry, null, indent))
+  fs.writeFileSync('../src/data/country-categories.json', JSON.stringify(regions, null, indent))
+
+  console.log(`
+    Written data files:
+      • ../src/data/rights-by-country.json: Rights data for each country
+      • ../src/data/country-categories.json: Countries grouped by category (geographical or political)
+  `)
+}
+
+calculate()
