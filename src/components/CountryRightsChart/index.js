1ad1f30d4437744f45558a40a436df68e22b3532
cesare.soldini <cesare.soldini@accurat.it>
2018-02-16 16:32:11 +0100

    🎨 Fix PetalChart labels in CountryPage

--- src/components/CountryRightsChart/index.js
+++ src/components/CountryRightsChart/index.js
@@ -99,64 +99,64 @@ function PetalLabels({ size, data, colors }) {
 
   return (
     <div style={{ fontSize: 14, color: '#606163' }}>
-      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 0} correction={[-45, -20]} style={{ textAlign: 'center' }}>
-        health
+      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 0} correction={[-50, -20]} style={{ textAlign: 'center' }}>
+        Health
         <br/>
         <strong style={{ color: colors[0] }}>{displayPercent(0)}</strong>
       </LabelRadial>
       <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 1} correction={[0, -20]} style={{ textAlign: 'left' }}>
-        housing
+        Housing
         <br/>
         <strong style={{ color: colors[1] }}>{displayPercent(1)}</strong>
       </LabelRadial>
       <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 2} correction={[0, -15]} style={{ textAlign: 'left' }}>
-        work
+        Work
         <br/>
         <strong style={{ color: colors[2] }}>{displayPercent(2)}</strong>
       </LabelRadial>
       <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 3} correction={[-10, -20]} style={{ textAlign: 'left' }}>
-        freedom from<br/>disappearance
+        Freedom from disappearance
         <br/>
         <strong style={{ color: colors[3] }}>{displayTenth(3)}</strong>
       </LabelRadial>
       <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 4} correction={[-10, -10]} style={{ textAlign: 'left' }}>
-        freedom from<br/>arbitrary arrest
+        Freedom from arbitrary arrest
         <br/>
         <strong style={{ color: colors[4] }}>{displayTenth(4)}</strong>
       </LabelRadial>
       <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 5} correction={[0, -15]} style={{ textAlign: 'left' }}>
-        freedom<br/>from execution
+        Freedom from execution
         <br/>
         <strong style={{ color: colors[5] }}>{displayTenth(5)}</strong>
       </LabelRadial>
-      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 6} correction={[-52, -15]} style={{ textAlign: 'center' }}>
-        freedom<br/>from torture
+      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 6} correction={[-52, -20]} style={{ textAlign: 'center' }}>
+        Freedom from torture
         <br/>
         <strong style={{ color: colors[6] }}>{displayTenth(6)}</strong>
       </LabelRadial>
 
-      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 7} correction={[-115, -15]} style={{ textAlign: 'right' }}>
-        participate<br/>in government
+      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 7} correction={[-105, -15]} style={{ textAlign: 'right' }}>
+        Participate in government
         <br/>
         <strong style={{ color: colors[7] }}>{displayTenth(7)}</strong>
       </LabelRadial>
-      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 8} correction={[-105, -10]} style={{ textAlign: 'right' }}>
-        assembly<br/>and association
+      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 8} correction={[-100, -10]} style={{ textAlign: 'right' }}>
+        Assembly and association
         <br/>
         <strong style={{ color: colors[8] }}>{displayTenth(8)}</strong>
       </LabelRadial>
       <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 9} correction={[-95, -20]} style={{ textAlign: 'right' }}>
-        opinion<br/>and expression
+        Opinion and expression
         <br/>
         <strong style={{ color: colors[9] }}>{displayTenth(9)}</strong>
       </LabelRadial>
-      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 10} correction={[-80, -15]} style={{ textAlign: 'right' }}>
-        food
+      <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 10} correction={[-105, -15]} style={{ textAlign: 'right' }}>
+        Food
         <br/>
         <strong style={{ color: colors[10] }}>{displayPercent(10)}</strong>
       </LabelRadial>
       <LabelRadial surfaceSize={size} r={size / 4 + 30} a={360 / 12 * 11} correction={[-110, -20]} style={{ textAlign: 'right' }}>
-        education
+        Education
         <br/>
         <strong style={{ color: colors[11] }}>{displayPercent(11)}</strong>
       </LabelRadial>
@@ -166,7 +166,7 @@ function PetalLabels({ size, data, colors }) {
 
 function Label({ x, y, children, ...props }) {
   return (
-    <div { ...props } style={{ ...(props.style || {}), position: 'absolute', top: y, left: x }}>
+    <div { ...props } style={{ ...(props.style || {}), position: 'absolute', top: y, left: x, width: 100 }}>
       {children}
     </div>
   )
