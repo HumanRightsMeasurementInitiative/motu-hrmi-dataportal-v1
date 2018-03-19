1a2055a04830aefa99d4f1fefe4a34d6707e4ed6
Gabriele Lippi <gabriele.lippi@accurat.it>
2018-03-19 12:43:18 +0100

    Delete informations on ESR right selected right pane

--- src/components/DefinitionFooter/index.js
+++ src/components/DefinitionFooter/index.js
@@ -26,28 +26,33 @@ export default class RightsPage extends React.Component {
 
     return (
       <div>
-        { !isESRSelected && cloudWords.length !== 0
-          ? (
-            <div>
-              <QuestionTooltip width={214} question={content.cpr_at_risk.title} isTitle={true}>
-                <p>{content.cpr_at_risk.tooltip} <a href='https://humanrightsmeasurement.org/wp-content/uploads/2018/03/Qualitative-responses-HRMI-2017-pilot.pdf' target='_blank'>{content.cpr_at_risk.link}</a>.</p>
-              </QuestionTooltip>
-              <WordCloudChart words={cloudWords} content={content}/>
-            </div>
-          )
-          : (
-            <div>
-              <div className={styles.CPRTitle}>{content.cpr_at_risk.title}</div>
-              <div style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }}>None Selected by Expert Respondents</div>
-            </div>
-          )
-        }
-        { isESRSelected && currCountry && !isHideTimeline &&
-          <div>
-            <div className={styles.subtitleESR}>{content.esr_trend.title}</div>
-            <div className={styles.esrChartKey}>{content.esr_trend.subtitle}</div>
-          </div>
-        }
+        {(isESRSelected && currCountry && !isHideTimeline) &&
+        <div>
+          <div className={styles.subtitleESR}>{content.esr_trend.title}</div>
+          <div className={styles.esrChartKey}>{content.esr_trend.subtitle}</div>
+        </div>
+            }
+        {!isESRSelected && cloudWords.length !== 0 &&
+        <div>
+          <QuestionTooltip width={214} question={content.cpr_at_risk.title} isTitle={true}>
+            <p>{content.cpr_at_risk.tooltip}
+              <a
+                href='https://humanrightsmeasurement.org/wp-content/uploads/2018/03/Qualitative-responses-HRMI-2017-pilot.pdf'
+                target='_blank'
+                                >
+                {content.cpr_at_risk.link}.
+                                 </a>
+            </p>
+          </QuestionTooltip>
+          <WordCloudChart words={cloudWords} content={content}/>
+        </div>
+              }
+        {!isESRSelected && cloudWords.length === 0 &&
+        <div>
+          <div className={styles.CPRTitle}>{content.cpr_at_risk.title}</div>
+          <div style={{ fontWeight: 'bold', fontSize: '15px', textAlign: 'center' }}>None Selected by Expert Respondents</div>
+        </div>
+         }
       </div>
     )
   }
--- src/components/RightBarchart/CPRRects.js
+++ src/components/RightBarchart/CPRRects.js
@@ -42,8 +42,8 @@ export default class CPRRects extends React.Component {
         onClick={this.onClick}
         onMouseOver={this.onMouseOver}
         onMouseOut={this.onMouseOut}
-        opacity={currCountry === country || !currCountry ? 1 : 0.5}>
-        {/* <rect className="-bar-background" y={-value} height={value} width='9' x='-4.5' fill={isActive ? '#abc1d9' : 'rgba(0, 0, 0, .1)'}></rect> */}
+        opacity={currCountry === country || !currCountry ? 1 : 0.5}
+       >
         <rect className="-bar-min-max" y={-maxValue} height={maxValue - minValue} width='9' x='-4.5' fill='#3378ae'></rect>
         <rect className="-bar-center" y={-value - 1} height='2' width='9' x='-4.5' fill='#fff'></rect>
       </g>
